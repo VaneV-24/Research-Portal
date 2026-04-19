48699e16c655c67ec079a5e440f69cd138d390a5
Vanessa <139142710+VaneV-24@users.noreply.github.com>
2026-04-18 21:56:42 -0400

    Delete team-routerunners-main directory

--- team-routerunners-main/faculty_data.py
+++
@@ -1,4 +0,0 @@
-faculty = [{'firstname':'MAC','lastname':'MAC', 'email':'mcmarshall888@gmail.com'},
-          {'firstname':'jake','lastname':'clay', 'email':'jakeclaybrook997@gmail.com'},
-          {'firstname':'Vanessa','lastname':'V', 'email':'vanessa.g.villalba@gmail.com'},
-          {'firstname':'Vanessa','lastname':'V', 'email':'vvillalba@wpi.edu'}]
